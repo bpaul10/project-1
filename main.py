ae3610812fa71ad8710bd4c4d9759a5692770553
bernard <bpaul10@student.cscc.edu>
2026-09-24 11:42:11 -0400

    created the player options of action and what they will  do.

--- main.py
+++ main.py
@@ -39,3 +39,41 @@ boss_name = random.choice(list(bosses))
 boss_hp, boss_attack, boss_special = bosses[boss_name]
 
 print("Your character is ", player, "and your opponent is ", boss_name)
+
+#the battle phase
+while player_hp > 0 and boss_hp > 0:
+
+    print("The battle has begun")
+    print(player, "HP: ", player_hp)
+    print(boss_name, "HP: ", boss_hp)
+    print()
+    print("Your options are; 1 for attack, 2 for special attack")
+    print("3 to heal, and 4 to quit")
+
+    action = input("Choose your action: ")
+
+    if action == "1":
+        #damage will be a random number inbetween the attack stat with a range of +-3
+        damage = random.randint(attack-3, attack+3) 
+        boss_hp -= damage
+        print("\nYou dealt", damage, "damage to ", boss_name)
+
+    elif action == "2":
+        damage = random.randint(attack, attack +10)
+        boss_hp -= damage
+        print("\n You used a special attack you dealt", damage, "damage to ", boss_name)
+
+    elif action == "3":
+        old_hp = player_hp
+        player_hp = min(player_hp + heal, max_hp)
+        print("\nYou healed ", player_hp - old_hp, "HP")
+
+    elif action == "4":
+        print("\n you ran away from the battle")
+        break
+
+    if boss_hp <= 0:
+        print("Yayy you defeated the boss, you win")
+        break
+
+    
